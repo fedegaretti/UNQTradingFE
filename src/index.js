e97cab985878b57179ba4853c31a1c1a1ddf18ca
fabianfrangella <fabianfrangella@gmail.com>
2020-05-31 23:56:13 -0300

    revert

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Test from './Components/Test';
 import * as serviceWorker from './serviceWorker';
 
