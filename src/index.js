414a6faeda30016cf69cb0000027cf1c9070deaf
fabianfrangella <fabianfrangella@gmail.com>
2020-06-01 18:54:32 -0300

    estilo en index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Test from './Components/Test';
 import * as serviceWorker from './serviceWorker';
 
