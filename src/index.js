9ca48224ad6f1eca0a034f86fb11f27a7f6b2624
fabianfrangella <fabianfrangella@gmail.com>
2020-06-01 18:34:57 -0300

    agregado estilo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 import Test from './Components/Test';
 import * as serviceWorker from './serviceWorker';
 
