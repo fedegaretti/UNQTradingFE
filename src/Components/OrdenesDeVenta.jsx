3750b548b876ea901ade6644e7cec5877270beac
fabianfrangella <fabianfrangella@gmail.com>
2020-06-07 03:14:02 -0300

    cambiado titulo a ordenes de venta

--- src/Components/OrdenesDeVenta.jsx
+++ src/Components/OrdenesDeVenta.jsx
@@ -7,7 +7,7 @@ export default function OrdenesDeVenta()  {
 return (
         <div>
             <div className="bg-secondary row justify-content-center mb-4">
-                <h3 className="text-white"> Visualización de Acciones </h3>
+                <h3 className="text-white"> Ordenes de Venta</h3>
             </div>
             <div className="container padding-top">
                 <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '80vh'}}>
