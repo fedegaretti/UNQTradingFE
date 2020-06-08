b313e4979b0236d163951f64a7106204e81ff82e
Fabian Frangella <fabianfrangella@gmail.com>
2020-06-07 21:36:10 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom'
-import Venta from './Components/Venta.jsx'
 import OrdenesDeVenta from './Components/OrdenesDeVenta.jsx'
 
 class App extends Component {
