a9bcef4b0055368ccf5882eb54e51688dd3051e2
Fabian Frangella <fabianfrangella@gmail.com>
2020-06-10 01:11:04 -0300

    from provisorio

--- src/Components/OrdenDeVenta.jsx
+++ src/Components/OrdenDeVenta.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {comprarAcciones, findOrdenDeVenta} from '../Service/RestService'
+import { Button, Form, InputGroup, Alert } from 'react-bootstrap';
 export default function OrdenDeVenta() {
     const [orden, setOrden] = useState('')
 
@@ -9,17 +10,28 @@ export default function OrdenDeVenta() {
 
     return (
         <div>
-            <h1>Empresa: {orden.nombreEmpresa}</h1>
-            <h1>Cantidad de Acciones: {orden.cantidadDeAcciones}</h1>
-            <h1>Precio: {orden.precio}</h1>
-            <h1>Fecha de Creacion: {orden.fechaDeCreacion}</h1>
-            <h1>Fecha de Vencimiento: {orden.fechaDeVencimiento}</h1>
+            <Form>
+                <label>Empresa: </label>
+                <Form.Control type="text" defaultValue={orden.nombreEmpresa} readOnly/>
+                <label>Cantidad de acciones: </label>
+                <Form.Control type="number" defaultValue={orden.cantidadDeAcciones} readnOnly/>
+                <label>Precio: </label>
+                <Form.Control type="number" defaultValue={orden.precio} readOnly/>
+                <label>Fecha de Creacion: </label>
+                <Form.Control type="date" defaultValue={orden.fechaDeCreacion} readOnly/>
+                <label>Fecha de Vencimiento: </label>
+                <Form.Control type="date" defaultValue={orden.fechaDeVencimiento} readOnly/>
+            </Form>
+            <Button variant="primary" onClick={event => comprar(event, orden.id)}>
+                    Comprar
+            </Button>
         </div>
     )
 }
 
-function comprar() {
-    comprarAcciones(1, orden.id)
+function comprar(event, id) {
+    event.preventDefault()
+    comprarAcciones(1, id)
 }
 
 
