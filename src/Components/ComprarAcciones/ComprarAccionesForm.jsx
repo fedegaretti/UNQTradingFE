18ba72c8a58aa679b5bcb5e5a13dcd97aa2287d4
fabianfrangella <fabianfrangella@gmail.com>
2020-06-12 10:24:00 -0300

    Agregado dialog de terminos y condiciones

--- src/Components/ComprarAcciones/ComprarAccionesForm.jsx
+++ src/Components/ComprarAcciones/ComprarAccionesForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { comprarAcciones, findOrdenDeVenta } from '../../Service/RestService'
 import { makeStyles } from '@material-ui/core/styles';
-import { TextField, Button } from '@material-ui/core';
+import { TextField, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
 import { Alert } from '@material-ui/lab';
 import { Checkbox, FormControlLabel } from '@material-ui/core';
 
@@ -19,6 +19,7 @@ export default function ComprarAccionesForm(props) {
     const [orden, setOrden] = useState('')
     const [alert, setAlert] = useState({ show: false, variant: "danger", message: '', icon: false });
     const [accept, setAccept] = useState(false)
+    const [show, setShow] = useState(false)
 
     const classes = useStyles();
     useEffect(() => {
@@ -28,6 +29,14 @@ export default function ComprarAccionesForm(props) {
     function handleAccept(event) {
         setAccept(event.target.checked)
     }
+
+    const handleShow = () => () => {
+        setShow(true);
+      };
+    
+      const handleClose = () => {
+        setShow(false);
+      };
 
     function comprar(ev, ordenId) {
         ev.preventDefault()
@@ -93,8 +102,10 @@ export default function ComprarAccionesForm(props) {
                     variant="outlined" />
             </form>
             <FormControlLabel className="p-2"
-                control={<Checkbox checked={accept} onChange={event => handleAccept(event)} />}
-                label="Acepto los terminos y condiciones" />
+                control={<Checkbox checked={accept} onChange={event => handleAccept(event)}/>}
+                label={<Button style= {{ textTransform:'none'}} value="justify" onClick={handleShow()}>
+                            Acepto los terminos y Condiciones
+                        </Button>}/>
             <div>
             </div>
             <div>
@@ -105,6 +116,26 @@ export default function ComprarAccionesForm(props) {
             <Alert className="mt-2" variant={alert.variant} severity={alert.severity} icon={alert.icon}>
                 {alert.message}
             </Alert>
+            <Dialog
+                open={show}
+                onClose={handleClose}
+                scroll="body">
+                <DialogTitle id="scroll-dialog-title">Terminos y Condiciones</DialogTitle>
+                <DialogContent dividers={true}>
+                    <DialogContentText
+                        id="scroll-dialog-description">
+                            Cras mattis consectetur purus sit amet fermentum.
+                            Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+                            Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cerrar
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     )
 }
