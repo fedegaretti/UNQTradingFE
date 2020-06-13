4aed8f16422363457cda7108b906f90ff971ce63
Fabian Frangella <fabianfrangella@gmail.com>
2020-06-13 14:14:22 -0300

    fix position footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -29,6 +29,9 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: theme.spacing(2),
   },
   footer: {
+    position: 'absolute',
+    bottom: '0',
+    width: '100%',
     padding: theme.spacing(3, 2),
     marginTop: 'auto',
     backgroundColor:
