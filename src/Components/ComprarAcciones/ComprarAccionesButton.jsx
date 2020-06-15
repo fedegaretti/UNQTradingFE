7a08bdd06b3f8716ce4b059cddbe2c8d3f11081a
Fabian Frangella <fabianfrangella@gmail.com>
2020-06-15 17:35:42 -0300

    eliminadas funciones innecesarias

--- src/Components/ComprarAcciones/ComprarAccionesButton.jsx
+++ src/Components/ComprarAcciones/ComprarAccionesButton.jsx
@@ -6,28 +6,26 @@ import {properties} from "../../Properties/properties.js"
 import {PaperDraggable} from "../MaterialDesign/PaperDraggable.jsx"
 
 export default function ComprarAccionesButton(props) {
-    const handleShow = () => setShow(true);
-    const handleClose = () => setShow(false);
     const [show, setShow] = useState(false);
     // hardcodeada la orden y el usuario, cuando se implementa con la lista se pasa por props
     const body = (<ComprarAccionesForm ordenId={1} usuarioId={1} />)
 
     return (
         <div>
-            <Button variant="contained" color="primary" onClick={handleShow}>
+            <Button variant="contained" color="primary" onClick={() => setShow(true)}>
                 {properties.labels.comprar}
             </Button>
             <Dialog
                 color="primary"
                 open={show}
-                onClose={handleClose}
+                onClose={() => setShow(false)}
                 PaperComponent={PaperDraggable}>
                 <DialogTitle disableTypography style={{
                     cursor: 'move', display: 'flex',
                     justifyContent: 'space-between', alignItems: 'center'
                 }} id="draggable-dialog-title">
                     <h3>{properties.labels.comprarAcciones}</h3>
-                    <IconButton onClick={handleClose}>
+                    <IconButton onClick={() => setShow(false)}>
                         <CloseIcon />
                     </IconButton>
                 </DialogTitle>
