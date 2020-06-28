8d8bbbcff4261f1284371632a4b8df93ef603fd5
Juan Cruz Vincenti <juancruzvincenti@gmail.com>
2020-06-28 12:22:07 -0300

    Agregado el margen izquierdo y la concatenacion del saldo

--- src/Components/Saldo.jsx
+++ src/Components/Saldo.jsx
@@ -1,19 +1,30 @@
 import React, {useEffect, useState} from 'react';
 import { RestService } from '../Service/RestService'
 import Typography from '@material-ui/core/Typography';
+import axios from "axios";
 
 export default function Saldo(props){
-    const [saldo, getSaldo] = useState()
+    const [saldo, setSaldo] = useState()
     const usuarioId = !!props.usuario ? props.usuario.id : '';
-
-    useEffect(() => {
+    const getSaldo = () => {
+        /*
             RestService.GET.findUser(usuarioId)
                 .then(response => getSaldo(response.data))
-        }, [])
+                */
+        axios.get("https://visualizarsaldo.free.beeceptor.com/")
+            .then(response => {
+                setSaldo(response.data.saldo)
+            })
+    }
+
+    useEffect(() => {
+            getSaldo()
+        }, []
+    )
 
     return(
-        <Typography variant="h6">
-            Saldo: ${saldo}
+        <Typography variant="h6" style={{marginLeft: "70vw"}}>
+            Saldo: {saldo}
         </Typography>
     )
 }
