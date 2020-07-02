2d3866e9dcfed6ba3666c0900abef0614a3b90ff
fedegaretti <fedegaretti@gmail.com>
2020-07-01 21:51:08 -0300

    cambiado el menu de seleccion

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {AppBar, Toolbar} from '@material-ui/core';
-import LoginDialog from "./LoginDialog";
-import RegistroDialog from "./RegistroDialog";
+import LoginMenu from "./LoginMenu";
+import RegistroMenu from "./RegistroMenu";
 
 
 export default function HomePage() {
@@ -9,8 +9,8 @@ export default function HomePage() {
     <AppBar position="static">
         <Toolbar>
             <img style={{marginLeft: "5vw"}} src={require('../../Images/logo300.png')} alt="UNQTrading Logo" width="130" height="115"/>
-            <LoginDialog/>
-            <RegistroDialog/>
+            <LoginMenu/>
+            <RegistroMenu/>
         </Toolbar>
     </AppBar>
     );
---
+++ src/Components/HomePage/RegistroMenu.jsx
@@ -0,0 +1,85 @@
+import React, {useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import PersonIcon from '@material-ui/icons/Person';
+import BusinessIcon from '@material-ui/icons/Business';
+import { useHistory } from "react-router-dom";
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { withStyles } from '@material-ui/core/styles';
+
+const StyledMenu = withStyles({
+  paper: {
+    border: '1px solid #d3d4d5',
+  },
+})((props) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'center',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'center',
+    }}
+    {...props}
+  />
+));
+
+
+
+export default function RegistroMenu() {
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const history = useHistory();
+
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const goRegistroPersona = () => {
+    history.push("/Registro");
+  };
+
+  const goRegistroEmpresa = () => {
+    history.push("/RegistrarEmpresa");
+  };
+
+  return (
+  <div>
+    <Fab style={{marginLeft: "1vw"}} variant="outlined" color="primary" onClick={handleClick}>
+        Registrarse
+    </Fab>
+    <StyledMenu
+        id="customized-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+    >
+        <MenuItem onClick={goRegistroPersona}>
+            <ListItemIcon>
+                <PersonIcon fontSize="small" />
+            </ListItemIcon>
+                <ListItemText primary="Persona" />
+        </MenuItem>
+        <MenuItem onClick={goRegistroEmpresa}>
+            <ListItemIcon>
+                <BusinessIcon fontSize="small" />
+            </ListItemIcon>
+                <ListItemText primary="Empresa" />
+        </MenuItem>
+    </StyledMenu>
+  </div>
+  );
+}
