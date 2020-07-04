ccf9341203e62a86ccdd7c8516355b83ea860471
fabianfrangella <fabianfrangella@gmail.com>
2020-07-04 14:49:41 -0300

    imagenes ajustadas al screen size

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -15,6 +15,10 @@ export default function HomePage() {
                         <img
                             src={require('../../Images/tradingunq1.png')}
                             alt="tradingunq1"
+                            style= {{ 
+                                maxWidth: "100%",
+                                height: "auto",
+                                width: "auto"}}
                         />
                         <Carousel.Caption>
                             <h2 style={{fontFamily: 'Playfair Display', textShadow: '0px 0px 3px #000000'}}>UNQ Trading | Inversiones al alcance de todos</h2>
@@ -24,6 +28,10 @@ export default function HomePage() {
                         <img
                             src={require('../../Images/trading3.jpg')}
                             alt="trading3"
+                            style= {{ 
+                                maxWidth: "100%",
+                                height: "auto",
+                                width: "auto"}}
                         />
                         <Carousel.Caption>
                             <h2 style={{fontFamily: 'Playfair Display', textShadow: '0px 0px 3px #000000'}}>UNQ Trading | El mercado de acciones nunca estuvo tan fácil</h2>
@@ -33,6 +41,10 @@ export default function HomePage() {
                         <img
                             src={require('../../Images/trading5.png')}
                             alt="trading5"
+                            style= {{ 
+                                maxWidth: "100%",
+                                height: "auto",
+                                width: "auto"}}
                         />
                         <Carousel.Caption>
                             <h2 style={{fontFamily: 'Playfair Display', textShadow: '0px 0px 3px #000000'}}>UNQ Trading | Tu lugar para invertir</h2>
